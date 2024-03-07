ee04aa8baf2c5f5167a381b8a5153ddb6d272649
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-03-07 19:43:37 +0600

    Update Contact Section

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,21 +2,6 @@ import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 
-const variants = {
-  initial: {
-    y: 500,
-    opacity: 0,
-  },
-  animate: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.5,
-      staggerChildren: 0.1,
-    },
-  },
-};
-
 const ContactPage = () => {
   const formRef = useRef();
 
@@ -44,73 +29,64 @@ const ContactPage = () => {
   };
 
   return (
-    <div className="py-20 bg-zinc-100 dark:bg-zinc-950">
-      <motion.div
-        variants={variants}
-        initial="initial"
-        animate="animate"
-        className="max-w-7xl m-auto flex flex-col md:flex-row items-center justify-center"
-      >
-        <motion.div variants={variants} className="w-full md:w-1/2 p-3">
-          <motion.div variants={variants}>
-            <h2 className="font-bold font-serif text-center md:text-left text-3xl md:text-4xl mb-8 text-indigo-700 dark:text-indigo-400">
-              Let's work together
-            </h2>
-            <p className="font-semibold text-center md:text-left text-lg md:text-xl tracking-wide text-blue-800 dark:text-blue-400">
-              Hate forms? Send me an{" "}
-              <a
-                title="toufiqjilanirabbu7@gmail.com"
-                href="mailto:toufiqjilanirabbu7@gmail.com"
-                className="underline text-emerald-800 dark:text-emerald-400"
-              >
-                email
-              </a>{" "}
-              instead.
-            </p>
-          </motion.div>
-          <motion.div variants={variants}>
-            <img
-              src="./mail.png"
-              alt="Mail"
-              className="max-w-32 py-5 m-auto md:m-0"
-            />
-          </motion.div>
-        </motion.div>
-
-        <div className="w-full md:w-1/2 p-3">
-          <form
-            ref={formRef}
-            onSubmit={sendEmail}
-            className="flex flex-col gap-4"
+    <div className="py-16 bg-zinc-100 dark:bg-zinc-950 flex justify-center items-center">
+      <div className="max-w-[500px] m-auto p-6 rounded shadow-[0px_0px_15px_10px] shadow-blue-700/5 dark:shadow-blue-400/5">
+        <h2 className="font-bold font-serif text-center text-3xl md:text-4xl mb-5 text-indigo-700 dark:text-indigo-400">
+          Let's work together
+        </h2>
+        <p className="font-semibold text-center text-lg md:text-xl tracking-wide text-blue-800 dark:text-blue-400">
+          Hate forms? Send me an{" "}
+          <a
+            title="toufiqjilanirabbu7@gmail.com"
+            href="mailto:toufiqjilanirabbu7@gmail.com"
+            className="underline text-emerald-800 dark:text-emerald-400"
           >
-            <input
-              type="text"
-              placeholder="Enter Name"
-              name="name"
-              required
-              className="bg-transparent p-4 outline-none tracking-wide rounded border border-solid text-blue-700 dark:text-blue-400 border-blue-700 dark:border-blue-400"
-            />
-            <input
-              type="email"
-              placeholder="Enter Email"
-              name="email"
-              required
-              className="bg-transparent p-4 outline-none tracking-wide rounded border border-solid text-blue-700 dark:text-blue-400 border-blue-700 dark:border-blue-400"
-            />
-            <textarea
-              rows={8}
-              name="message"
-              placeholder="Enter Message"
-              className="bg-transparent p-4 outline-none tracking-wide rounded border border-solid text-blue-700 dark:text-blue-400 border-blue-700 dark:border-blue-400"
-            />
-            <button className="p-4 font-bold tracking-wider rounded text-blue-100 dark:text-blue-700 bg-blue-700 dark:bg-blue-100">
-              Send Message
-            </button>
-            {error && "Error"}
-            {success && "Success"}
-          </form>
-        </div>
-      </motion.div>
+            email
+          </a>{" "}
+          instead.
+        </p>
+        <motion.img
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          src="./mail.png"
+          alt="Mail"
+          className="max-w-32 py-5 m-auto cursor-pointer"
+        />
+        <form
+          ref={formRef}
+          onSubmit={sendEmail}
+          className="flex flex-col gap-4"
+        >
+          <input
+            type="text"
+            placeholder="Enter Name"
+            name="name"
+            className="bg-transparent p-4 outline-none tracking-wide rounded border border-solid text-blue-700 dark:text-blue-400 border-blue-700 dark:border-blue-400"
+            required
+          />
+          <input
+            type="email"
+            placeholder="Enter Email"
+            name="email"
+            className="bg-transparent p-4 outline-none tracking-wide rounded border border-solid text-blue-700 dark:text-blue-400 border-blue-700 dark:border-blue-400"
+            required
+          />
+          <textarea
+            name="message"
+            placeholder="Enter Message"
+            className="bg-transparent p-4 outline-none tracking-wide rounded border border-solid text-blue-700 dark:text-blue-400 border-blue-700 dark:border-blue-400"
+          />
+          <motion.button
+            whileHover={{ scale: 0.96 }}
+            whileTap={{ scale: 1 }}
+            className="p-4 font-bold tracking-wider rounded text-blue-100 dark:text-blue-700 bg-blue-700 dark:bg-blue-100"
+          >
+            Send Message
+          </motion.button>
+          {error && "Error"}
+          {success && "Success"}
+        </form>
+      </div>
     </div>
   );
 };
