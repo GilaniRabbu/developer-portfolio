6a03c7b552594a918c026f100ebecf5a954f76d7
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-01-20 23:36:25 +0600

    updated footer section

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,6 +4,8 @@ import {
   FaXTwitter,
   FaLinkedinIn,
   FaFacebookF,
+  FaCodepen,
+  FaMastodon,
 } from "react-icons/fa6";
 
 export default function Footer() {
@@ -22,76 +24,48 @@ export default function Footer() {
     },
     {
       id: 3,
+      title: "Codepen",
+      link: "https://codepen.io/GilaniRabbu",
+      icon: <FaCodepen size={20} />,
+    },
+    {
+      id: 4,
+      title: "Mastodon",
+      link: "https://mastodon.social/@GilaniRabbu",
+      icon: <FaMastodon size={20} />,
+    },
+    {
+      id: 5,
       title: "Twitter",
       link: "https://x.com/GilaniRabbu",
       icon: <FaXTwitter size={20} />,
     },
     {
-      id: 4,
+      id: 6,
       title: "Facebook",
       link: "https://www.facebook.com/GilaniRabbu",
       icon: <FaFacebookF size={20} />,
     },
   ];
 
-  const footerNav = [
-    {
-      id: 1,
-      title: "About",
-      link: "#",
-    },
-    {
-      id: 2,
-      title: "Projects",
-      link: "#",
-    },
-    {
-      id: 3,
-      title: "Skill",
-      link: "#",
-    },
-    {
-      id: 4,
-      title: "Blog",
-      link: "#",
-    },
-    {
-      id: 5,
-      title: "Contact",
-      link: "#",
-    },
-  ];
-
   return (
-    <footer className="py-16 relative flex flex-col items-center gap-8 overflow-hidden bg-[#CBC9C9] dark:bg-[#292424]">
-      <div className="flex gap-4 md:gap-6">
+    <footer className="py-8 px-4 relative flex flex-col items-center gap-6 overflow-hidden bg-[#CBC9C9] dark:bg-[#292424]">
+      <div className="flex flex-wrap justify-center gap-1">
         {footerLinks.map((link) => (
           <a
             key={link.id}
             href={link.link}
             target="_blank"
             rel="noopener noreferrer"
-            className="p-2 border-2 rounded-full border-[#221E1E] dark:border-[#EEF2FF]"
+            className="p-2 rounded transition-all duration-300 hover:text-[#FFFFFF] hover:dark:text-[#221E1E] hover:bg-[#377E86] hover:dark:bg-[#659477]"
           >
+            <span className="sr-only">Social Links</span>
             {link.icon}
           </a>
         ))}
       </div>
-      <div className="h-[2px] w-full bg-[#221E1E] dark:bg-[#EEF2FF]"></div>
-      <nav className="mt-4 flex gap-4 md:gap-8 flex-col md:flex-row items-center">
-        {footerNav.map((item) => (
-          <a
-            key={item.id}
-            href={item.link}
-            className="relative pb-1 text-3xl font-bonny-bold footer-link after:bg-[#221E1E] dark:after:bg-[#EEF2FF]"
-          >
-            {item.title}
-          </a>
-        ))}
-      </nav>
-      <p className="text-center px-2">
-        © {new Date().getFullYear()} All rights reserved by{" "}
-        <span className="font-bold">Gilani Rabbu</span>
+      <p className="text-center text-2xl tracking-wide font-bonny-bold">
+        Gilani © {new Date().getFullYear()}
       </p>
     </footer>
   );
