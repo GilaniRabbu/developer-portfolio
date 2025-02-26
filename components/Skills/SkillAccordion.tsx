438428e0cd5f2674c33a85558514d2bc12a06102
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2025-02-27 01:45:24 +0600

    updated skills section

--- components/Skills/SkillAccordion.tsx
+++ components/Skills/SkillAccordion.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { LuBadgePlus } from "react-icons/lu";
 
 export type AccordionItem = {
   title: string;
@@ -20,14 +21,17 @@ const SkillAccordion: React.FC<AccordionProps> = ({ items }) => {
     <div>
       {items.map((item, index) => (
         <div key={index}>
-          <button
-            onClick={() => toggleAccordion(index)}
-            className="w-full text-left p-5 font-medium"
-          >
-            {item.title}
-          </button>
+          <div className="flex p-5 items-center justify-between">
+            <p className="text-xl">{item.title}</p>
+            <button
+              onClick={() => toggleAccordion(index)}
+              className="p-2 border rounded-full"
+            >
+              <LuBadgePlus size="24" />
+            </button>
+          </div>
           {openIndex === index && (
-            <div className="p-5 bg-[#659477] dark:bg-[#2D2726]">
+            <div className="px-5 pt-0 pb-5 bg-[#659477] dark:bg-[#2D2726]">
               {item.content}
             </div>
           )}
--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import {
   SiTypescript,
@@ -63,20 +64,20 @@ export default function Skills() {
       <div className="container mx-auto mt-20 px-4">
         <div className="flex justify-center items-center">
           <div className="flex flex-1 gap-5 flex-col">
-            <div className="grid grid-cols-[1.2fr,0.8fr] gap-5">
-              <div className="p-5 w-full rounded bg-accordion border">
+            <div className="grid grid-cols-1 md:grid-cols-[1.2fr,0.8fr] gap-5">
+              <div className="w-full rounded overflow-hidden bg-accordion">
                 <SkillAccordion items={frontend} />
               </div>
-              <div className="p-5 w-full rounded bg-accordion border">
+              <div className="w-full rounded overflow-hidden bg-accordion">
                 <SkillAccordion items={backend} />
               </div>
             </div>
 
-            <div className="grid grid-cols-[0.8fr,1.2fr] gap-5">
-              <div className="p-5 w-full rounded bg-accordion border">
+            <div className="grid grid-cols-1 md:grid-cols-[0.8fr,1.2fr] gap-5">
+              <div className="w-full rounded overflow-hidden bg-accordion">
                 <SkillAccordion items={tools} />
               </div>
-              <div className="p-5 w-full rounded bg-accordion border">
+              <div className="w-full rounded overflow-hidden bg-accordion">
                 <SkillAccordion items={tools} />
               </div>
             </div>
